4e3e16e153406105b779169b585b7087c0a90146
Ilya <pankovskiy.im@gmail.com>
2024-10-12 21:30:49 +0300

    broken tasks

---
+++ broken_solution.py
@@ -0,0 +1,22 @@
+'''
+correct broken code
+'''
+# 1
+# number = input()
+# number_2 = 10
+# print(number + number_2)
+
+# 2
+# s = int('привет!')
+# print(s * 2)
+
+# 3
+# number = int(input())
+# number2 = int(input())
+# print(number = number2)
+
+# 4
+# number = bool(input())
+# number_2 = int(input())
+# print(number == number_2)
+
