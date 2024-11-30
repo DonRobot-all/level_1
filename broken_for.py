0c2b310585968663cf6dd78d0eae4623b3e9c8b5
Ilya <pankovskiy.im@gmail.com>
2024-11-30 21:39:03 +0300

    broken_for

---
+++ broken_for.py
@@ -0,0 +1,20 @@
+for i in range(10, 1, 1):
+    print(i)
+
+
+for i in range(1, 10):
+    print(a)
+
+
+for number in range('привет'):
+    print(number)
+
+
+for f, a in range(10):
+    print(f, a)
+
+
+for a in [4,5,3,1,5]
+    print(a)
+
+
